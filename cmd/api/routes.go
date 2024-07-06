e664922b04783b54453730c2ff3c55b8dc46963f
jonghyeon <jadepark.dev@gmail.com>
2024-07-06 20:10:18 +0100

    feat: user activation

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -26,6 +26,7 @@ func (app *application) routes() http.Handler {
 
 	// users
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
 	// Return the httprouter instance.
 	return app.recoverPanic(app.rateLimit(router))
