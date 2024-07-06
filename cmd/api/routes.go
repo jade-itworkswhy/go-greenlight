c32c22315246113d30a1508e90e61e48df81f363
jonghyeon <jadepark.dev@gmail.com>
2024-07-06 14:25:58 +0100

    feat: user registration

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -17,11 +17,15 @@ func (app *application) routes() http.Handler {
 	// register routers
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
+	// movies
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
+
+	// users
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 
 	// Return the httprouter instance.
 	return app.recoverPanic(app.rateLimit(router))
